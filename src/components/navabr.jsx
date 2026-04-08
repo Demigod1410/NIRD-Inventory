3dbd8dbecbddab3116174ca6586970d337bb10b1
DemiGod <demigod14100@gmail.com>
2026-04-08 13:37:34 +0530

    Update Navbar icons and text for improved clarity

--- src/components/navabr.jsx
+++ src/components/navabr.jsx
@@ -98,11 +98,11 @@ export default function Navbar() {
                 </li>
                 <li>
                   <a className="dropdown-item py-2" href="/map-inventory-employee">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-plus me-2" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-badge me-2" viewBox="0 0 16 16">
                       <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-                      <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1h-1.5v1.5a.5.5 0 0 1-1 0V8H12a.5.5 0 0 1 0-1h1.5V5.5a.5.5 0 0 1 .5-.5"/>
+                      <path d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1h-1.5v1.5a.5.5 0 0 1-1 0V8H12a.5.5 0 0 1 0-1h1.5V5.5a.5.5 0 0 1 .5-.5"/>
                     </svg>
-                    MAP Inventory to Employee
+                    Map Inventory to Employee
                   </a>
                 </li>
                 <li>
@@ -218,7 +218,7 @@ export default function Navbar() {
                     className="bi bi-person-fill" 
                     viewBox="0 0 16 16"
                   >
-                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
                   </svg>
                 </div>
                 <span>Admin User</span>
